6ca628c1e486f651314c0bdafeaa5abad1a8badd
Sri Harsha Chilakapati <hello@goharsha.com>
2021-04-15 02:40:42 +0530

    Keep change in Observable if no listeners are set

--- Virtual Tourist/Virtual Tourist/Data/Sources/Observable.swift
+++ Virtual Tourist/Virtual Tourist/Data/Sources/Observable.swift
@@ -11,12 +11,23 @@ class Observable<T> {
     typealias ChangeListener = (T) -> Void
     
     private var listeners = [ChangeListener]()
+    private var pendingUpdate: T?
     
     func listenForChanges(_ listener: @escaping ChangeListener) {
+        if let pendingUpdate = pendingUpdate {
+            listener(pendingUpdate)
+            self.pendingUpdate = nil
+        }
+        
         listeners.append(listener)
     }
     
     func dispatchChange(changed: T) {
+        if listeners.isEmpty {
+            pendingUpdate = changed
+            return
+        }
+        
         listeners.forEach { listener in listener(changed) }
     }
 }
